bce280fa0b9aa27e299a6eeef19c7a91b57b3fc4
RLMurta <rafaellmurta@hotmail.com>
2024-03-02 12:24:27 -0300

    feat: create functions file where the functions logic will be kept

---
+++ utils/functions.py
@@ -0,0 +1,30 @@
+class Functions:
+    def __init__(self):
+        pass
+
+    def move(self, points, dx, dy):
+        pass
+
+    def rotate(self, points, angle):
+        pass
+
+    def resize(self, points, scale_x, scale_y):
+        pass
+
+    def reflex(self, points, axis):
+        pass
+
+    def dda(self, x1, y1, x2, y2):
+        pass
+
+    def bres(self, x1, y1, x2, y2):
+        pass
+
+    def cohen_sutherland(self, x1, y1, x2, y2, x_min, y_min, x_max, y_max):
+        pass
+
+    def lian_barsky(self, x1, y1, x2, y2, x_min, y_min, x_max, y_max):
+        pass
+
+    def circle(self, x, y, r):
+        pass
